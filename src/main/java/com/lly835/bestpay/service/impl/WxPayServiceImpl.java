663cb39b535dd94bd6db63928a51e5c3905485bc
zicheng <602789583@qq.com>
2020-10-14 14:49:13 +0800

    分离微信付款码支付

--- src/main/java/com/lly835/bestpay/service/impl/WxPayServiceImpl.java
+++ src/main/java/com/lly835/bestpay/service/impl/WxPayServiceImpl.java
@@ -10,6 +10,7 @@ import com.lly835.bestpay.model.*;
 import com.lly835.bestpay.model.wxpay.WxPayApi;
 import com.lly835.bestpay.model.wxpay.request.*;
 import com.lly835.bestpay.model.wxpay.response.*;
+import com.lly835.bestpay.service.impl.wx.WxPayMicroServiceImpl;
 import com.lly835.bestpay.utils.MapUtil;
 import com.lly835.bestpay.utils.MoneyUtil;
 import com.lly835.bestpay.utils.RandomUtil;
@@ -38,9 +39,9 @@ import java.util.Map;
 @Slf4j
 public class WxPayServiceImpl extends BestPayServiceImpl {
 
-    private WxPayConfig wxPayConfig;
+    protected WxPayConfig wxPayConfig;
 
-    private final Retrofit retrofit = new Retrofit.Builder()
+    protected final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(WxPayConstants.WXPAY_GATEWAY)
             .addConverterFactory(SimpleXmlConverterFactory.create())
             .client(new OkHttpClient.Builder()
@@ -50,7 +51,7 @@ public class WxPayServiceImpl extends BestPayServiceImpl {
             )
             .build();
 
-    private final Retrofit devRetrofit = new Retrofit.Builder()
+    protected final Retrofit devRetrofit = new Retrofit.Builder()
             .baseUrl(WxPayConstants.WXPAY_GATEWAY_SANDBOX)
             .addConverterFactory(SimpleXmlConverterFactory.create())
             .client(new OkHttpClient.Builder()
@@ -68,7 +69,9 @@ public class WxPayServiceImpl extends BestPayServiceImpl {
     @Override
     public PayResponse pay(PayRequest request) {
         if (request.getPayTypeEnum() == BestPayTypeEnum.WXPAY_MICRO) {
-            return this.micropay(request);
+            WxPayMicroServiceImpl wxPayMicroService = new WxPayMicroServiceImpl();
+            wxPayMicroService.setWxPayConfig(wxPayConfig);
+            return wxPayMicroService.pay(request);
         }
         WxPayUnifiedorderRequest wxRequest = new WxPayUnifiedorderRequest();
         wxRequest.setOutTradeNo(request.getOrderId());
@@ -125,58 +128,6 @@ public class WxPayServiceImpl extends BestPayServiceImpl {
         }
 
         return buildPayResponse(response);
-    }
-
-    /**
-     * 微信付款码支付
-     * 提交支付请求后微信会同步返回支付结果。
-     * 当返回结果为“系统错误 {@link WxPayConstants#SYSTEMERROR}”时，商户系统等待5秒后调用【查询订单API {@link WxPayServiceImpl#query(OrderQueryRequest)}】，查询支付实际交易结果；
-     * 当返回结果为“正在支付 {@link WxPayConstants#USERPAYING}”时，商户系统可设置间隔时间(建议10秒)重新查询支付结果，直到支付成功或超时(建议30秒)；
-     *
-     * @param request
-     * @return
-     */
-    private PayResponse micropay(PayRequest request) {
-        WxPayUnifiedorderRequest wxRequest = new WxPayUnifiedorderRequest();
-        wxRequest.setOutTradeNo(request.getOrderId());
-        wxRequest.setTotalFee(MoneyUtil.Yuan2Fen(request.getOrderAmount()));
-        wxRequest.setBody(request.getOrderName());
-        wxRequest.setOpenid(request.getOpenid());
-        wxRequest.setAuthCode(request.getAuthCode());
-
-        wxRequest.setAppid(wxPayConfig.getAppId());
-        wxRequest.setMchId(wxPayConfig.getMchId());
-        wxRequest.setNonceStr(RandomUtil.getRandomStr());
-        wxRequest.setSpbillCreateIp(StringUtils.isEmpty(request.getSpbillCreateIp()) ? "8.8.8.8" : request.getSpbillCreateIp());
-        wxRequest.setAttach(request.getAttach());
-        wxRequest.setSign(WxPaySignature.sign(MapUtil.buildMap(wxRequest), wxPayConfig.getMchKey()));
-
-        //对付款码支付无用的字段
-        wxRequest.setNotifyUrl("");
-        wxRequest.setTradeType("");
-
-        RequestBody body = RequestBody.create(MediaType.parse("application/xml; charset=utf-8"), XmlUtil.toString(wxRequest));
-
-        WxPayApi api = null;
-        if (wxPayConfig.isSandbox()) {
-            api = devRetrofit.create(WxPayApi.class);
-        } else {
-            api = retrofit.create(WxPayApi.class);
-        }
-
-        Call<WxPaySyncResponse> call = api.micropay(body);
-
-        Response<WxPaySyncResponse> retrofitResponse = null;
-        try {
-            retrofitResponse = call.execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assert retrofitResponse != null;
-        if (!retrofitResponse.isSuccessful()) {
-            throw new RuntimeException("【微信付款码支付】发起支付, 网络异常");
-        }
-        return buildPayResponse(retrofitResponse.body());
     }
 
     @Override
@@ -376,7 +327,7 @@ public class WxPayServiceImpl extends BestPayServiceImpl {
      * @param response
      * @return
      */
-    private PayResponse buildPayResponse(WxPaySyncResponse response) {
+    protected PayResponse buildPayResponse(WxPaySyncResponse response) {
         String timeStamp = String.valueOf(System.currentTimeMillis() / 1000);
         String nonceStr = RandomUtil.getRandomStr();
         String prepayId = response.getPrepayId();
